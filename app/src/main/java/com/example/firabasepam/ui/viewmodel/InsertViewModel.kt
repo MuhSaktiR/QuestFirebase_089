96f7bf7cd59aeac8678657283430ba5d5420bc14
MuhSaktiR <seken876@gmail.com>
2025-01-11 22:19:04 +0700

    Membuat  data class FormErrorState untuk validasi formulir mahasiswa

--- app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,20 @@ package com.example.firabasepam.ui.viewmodel
 
 import com.example.firabasepam.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 //data class variable yang menyimpan data input form
 data class MahasiswaEvent(
     val nim: String = "",
