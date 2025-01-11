31ba7b9d9d059938b486bf9167d0875b3152d6c8
MuhSaktiR <seken876@gmail.com>
2025-01-11 22:23:46 +0700

    Membuat sealed class FormState untuk status formulir

--- app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,13 @@ package com.example.firabasepam.ui.viewmodel
 
 import com.example.firabasepam.model.Mahasiswa
 
+sealed class FormState {
+    object Idle: FormState()
+    object Loading: FormState()
+    data class Success(val message: Mahasiswa): FormState()
+    data class Error(val message: String): FormState()
+}
+
 data class InsertUiState (
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState()
