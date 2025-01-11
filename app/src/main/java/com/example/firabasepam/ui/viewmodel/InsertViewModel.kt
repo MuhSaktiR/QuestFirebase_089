4aa68954fcf82c3353dd2f7b8bac2f1d4cf0479f
MuhSaktiR <seken876@gmail.com>
2025-01-11 22:16:09 +0700

    Membuat data class variable yang menyimpan data input form

--- app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firabasepam/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,16 @@ package com.example.firabasepam.ui.viewmodel
 
 import com.example.firabasepam.model.Mahasiswa
 
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 //Menyimpan input form ke dalam entity
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa (
     nim = nim,
